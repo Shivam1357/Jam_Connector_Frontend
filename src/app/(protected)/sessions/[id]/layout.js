261479f83e642d5c5ff41dbc85982ca9f5ebca4b
Shivam Kumar Jaiswal <shivamjaiswal9628@gmail.com>
2025-10-15 01:57:15 +0530

    final done

---
+++ src/app/(protected)/sessions/[id]/layout.js
@@ -0,0 +1,9 @@
+import { Suspense } from "react";
+
+export default function IdSessionsLayout({ children }) {
+  return (
+    <Suspense fallback={<p>Loading Sessions page...</p>}>
+      {children}
+    </Suspense>
+  );
+}
---
+++ src/app/(protected)/sessions/layout.js
@@ -0,0 +1,9 @@
+import { Suspense } from "react";
+
+export default function SessionsLayout({ children }) {
+  return (
+    <Suspense fallback={<p>Loading Sessions page...</p>}>
+      {children}
+    </Suspense>
+  );
+}
