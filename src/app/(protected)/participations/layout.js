d0c5f0b8429bb33140bb42356ce8351eede1672f
Shivam Kumar Jaiswal <shivamjaiswal9628@gmail.com>
2025-10-15 01:53:29 +0530

    final done

---
+++ src/app/(protected)/participations/layout.js
@@ -0,0 +1,9 @@
+import { Suspense } from "react";
+
+export default function ParticipationsLayout({ children }) {
+  return (
+    <Suspense fallback={<p>Loading Participations page...</p>}>
+      {children}
+    </Suspense>
+  );
+}
