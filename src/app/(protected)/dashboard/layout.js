c13ca5d0f23ea3a42b0fffe8209341f4becb97cf
Shivam Kumar Jaiswal <shivamjaiswal9628@gmail.com>
2025-10-15 01:50:26 +0530

    final done

---
+++ src/app/(protected)/dashboard/layout.js
@@ -0,0 +1,9 @@
+import { Suspense } from "react";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <Suspense fallback={<p>Loading Dashboard page...</p>}>
+      {children}
+    </Suspense>
+  );
+}
